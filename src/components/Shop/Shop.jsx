b6540720797f24e852f9e441c9b97f4d70cd1634
Meherab Hosssain Bhuiyan <127032892+Meherab003@users.noreply.github.com>
2024-06-12 11:33:37 +0600

    order list customization

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -11,20 +11,22 @@ const Shop = () => {
     const {count} = useLoaderData();
     const [itemsPerPage, setItemsPerPage] = useState(10); 
     const numberOfPages = Math.ceil(count/ itemsPerPage)
-    console.log(count)
+    const [currentPage, setCurrentPage] = useState(0);
+    // console.log(count)
 
     const pages = [...Array(numberOfPages).keys()];
 
     /**
      * DONE: get the total number of products
-     * TODO 2: number of items per pages dynamic
+     * DONE: number of items per pages dynamic
+     * TODO: get the current page
      */
 
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5000/products?page=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [currentPage, itemsPerPage]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
@@ -71,6 +73,25 @@ const Shop = () => {
         setCart([]);
         deleteShoppingCart();
     }
+    
+    const handleItemsPerPage = e => {
+        const val = parseInt(e.target.value);
+        setItemsPerPage(val)
+        console.log(val)
+        setCurrentPage(0)
+    } 
+
+    const handlePreviousPage = () => {
+        if(currentPage > 0){
+            setCurrentPage(currentPage - 1)
+        }
+    }
+
+    const handleNextPage = () => {
+        if(currentPage < pages.length - 1){
+            setCurrentPage(currentPage + 1)
+        }
+    }
 
     return (
         <div className='shop-container'>
@@ -94,15 +115,21 @@ const Shop = () => {
                 </Cart>
             </div>
             <div className='p-2 bg-gray-800 rounded-xl mb-10 flex justify-between gap-1 min:w-1/3 mx-auto'>
+            {/* <p>Current page: {currentPage}</p> */}
+            <button className='btn' onClick={handlePreviousPage}>Previous</button>
                 {
-                    pages.map(page => <button className='btn' key={page}>{page}</button>)
+                    pages.map(page => <button className={`btn ${currentPage === page ? 'bg-orange-600' : undefined}`}
+                        onClick={() => setCurrentPage(page)}
+                         key={page}
+                         >{page}</button>)
                 }
-                <select className='rounded-lg dropdown' vlaue={itemsPerPage} name="" id="">
+                <select onChange={handleItemsPerPage} className='rounded-lg dropdown' value={itemsPerPage} name="" id="">
                     <option value="5">5</option>
                     <option value="10">10</option>
                     <option value="20">20</option>
                     <option value="50">50</option>
                 </select>
+                <button className='btn' onClick={handleNextPage}>Next</button>
             </div>
         </div>
     );
